8d62283c5555382f78ef1e90c8cd0cbaf572730c
TheLoyalStreamer <74324855+TheLoyalStreamer@users.noreply.github.com>
2023-01-04 07:58:47 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+# Create the empty message buffer file and give the webserver read and write permission.
+# The chmod command below gives every one read and write access (because I don't know
+# your setup) but if possible only give the webserver read and write access.
+touch messages.json
+chmod ugo=rw messages.json
